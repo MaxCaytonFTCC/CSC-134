ca95fcb0792fe97d1a0acb41da929a6527d32deb
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2024-10-21 12:41:40 +0000

    Added M5T1

---
+++ M5T1/M5T1.cpp
@@ -0,0 +1,30 @@
+// CSC 134 
+// M5T1
+// Max Cayton
+// 10/21/24
+
+#include <iostream> 
+#include <string> 
+using namespace std; 
+
+string formatAnswer(int answer); 
+void printAnswer(string msg); 
+
+int main() { 
+    int answer = 5; 
+    string message; 
+    message = formatAnswer(answer); 
+    printAnswer(message); 
+    return 0; 
+} 
+
+string formatAnswer (int answer) { 
+    string answerMessage; 
+    answerMessage = "The answer is "; 
+    answerMessage += to_string(answer); 
+    return answerMessage; 
+} 
+
+void printAnswer(string msg) { 
+  cout << msg << endl; 
+} 
